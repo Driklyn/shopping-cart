e7563e841a282289ae1aef47dd7339c6e955c894
Kevin Jurkowski <driklyn@gmail.com>
2019-02-01 00:17:25 -0600

    Updates

---
+++ precache-manifest.5ee776ce6776d49e52e75db7f9a99944.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b509cb4f645738d95a2a",
+    "url": "/shopping-cart/static/js/runtime~main.b509cb4f.js"
+  },
+  {
+    "revision": "a122e3248fc04561230e",
+    "url": "/shopping-cart/static/js/main.a122e324.chunk.js"
+  },
+  {
+    "revision": "167408b17f877dd8b3fb",
+    "url": "/shopping-cart/static/js/1.167408b1.chunk.js"
+  },
+  {
+    "revision": "a122e3248fc04561230e",
+    "url": "/shopping-cart/static/css/main.7666207a.chunk.css"
+  },
+  {
+    "revision": "48a7bb2672409c3109ba7e45a6794008",
+    "url": "/shopping-cart/index.html"
+  }
+];
